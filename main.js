5f828e96abf23923ae9b8b845c8f15e4ce87a9d7
Anthony-S58 <tonysim01@hotmail.com>
2021-06-16 10:20:54 +0200

    défi terminé

---
+++ main.js
@@ -0,0 +1,15 @@
+window.onscroll = slideDown;
+function slideDown() {
+ // Votre code ici
+ var nbar = document.getElementById('navbar');
+ var winScroll = window.scrollY;
+//  console.log(intElemScrollTop);
+console.log(winScroll);
+
+if (winScroll > 100 ) {
+ nbar.setAttribute("style" , "top:0px");
+
+}else {
+    nbar.setAttribute("style" , "top:-56px");
+}
+}
